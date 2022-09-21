bc29d205d944a60da0df02177cc6bccae4e12229
Natalya Maximova <natka-31@mail.ru>
2022-09-21 23:50:11 +0500

    изменить порт бэка на 3000

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const {
 
 const NotFoundError = require('./Error/NotFoundError');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 app.use(helmet());
 
